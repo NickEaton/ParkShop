19c895a5f94f46a3b7b63b9e896884f44807bdd7
Nick <nickeaton99@gmail.com>
2020-08-24 15:51:34 -0700

    Implemented Abstract Class Component, added in its first subclass, Frame

--- src/app/java/components/Component.java
+++ src/app/java/components/Component.java
@@ -3,5 +3,45 @@ package app.java.components;
 
 // Nick Eaton
 // 8/24/2020
+// A class representing the basic fields and methods all bike components will have
+
+import java.io.File;
+
 public abstract class Component {
+
+    // 0 - 100 rating of how much wear the component has left
+    protected double wearPercent = 0;
+
+    // 3-tuple 0-100 rating of <XC, END, DH> performance 'factor', these will be a constant read from file
+    protected double fitness_XC = 0;
+    protected double fitness_END = 0;
+    protected double fitness_DH = 0;
+
+    // Display name of the component
+    protected String compName = "<Default>";
+
+    // Internal ID of this particular component
+    protected String compID = "<Default>";
+
+    // What section a given component fits on the bike
+    protected int sectionId = 0;
+
+    // How much longer/shorter the build time
+    protected double timeModifier = 1;
+
+    // The default cost of the component in USD before modifiers
+    protected double costUSD = 0;
+
+    //------------------------------------------------------------------------------------------------------//
+
+    // Getters
+    public abstract double getWearPercent();
+    public abstract double getFitXC();
+    public abstract double getFitDH();
+    public abstract double getFitEND();
+    public abstract String getCompName();
+    public abstract String getCompID();
+    public abstract int getSectionId();
+    public abstract double getTimeModifier();
+    public abstract double getCostUSD();
 }
---
+++ src/app/java/components/Frame.java
@@ -0,0 +1,72 @@
+package app.java.components;
+
+import java.io.File;
+
+public class Frame extends Component {
+
+    // - Constructors
+
+    // File Constructor
+    public Frame(File fileIn) {
+        //TODO: Read from file constructor implementation
+    }
+
+    // Default Constuctor
+    public Frame(double _wearPercent, double _fitness_XC, double _fitness_END, double _fitness_DH, String _compName, String _compID, double _timeModifier, double _costUSD) {
+        this.wearPercent = _wearPercent;
+        this.fitness_XC = _fitness_XC;
+        this.fitness_END = _fitness_END;
+        this.fitness_DH = _fitness_DH;
+        this.compName = _compName;
+        this.compID = _compID;
+        this.timeModifier = _timeModifier;
+        this.costUSD = _costUSD;
+
+        sectionId = 1;
+    }
+
+    @Override
+    public double getWearPercent() {
+        return this.wearPercent;
+    }
+
+    @Override
+    public double getFitXC() {
+        return this.fitness_XC;
+    }
+
+    @Override
+    public double getFitDH() {
+        return this.fitness_DH;
+    }
+
+    @Override
+    public double getFitEND() {
+        return this.fitness_END;
+    }
+
+    @Override
+    public String getCompName() {
+        return this.compName;
+    }
+
+    @Override
+    public String getCompID() {
+        return this.compID;
+    }
+
+    @Override
+    public int getSectionId() {
+        return this.sectionId;
+    }
+
+    @Override
+    public double getTimeModifier() {
+        return this.timeModifier;
+    }
+
+    @Override
+    public double getCostUSD() {
+        return this.costUSD;
+    }
+}
